1fc99c59c97d77e93559f634466a3a9abf40c869
yuramayer <votetopovorot2015@gmail.com>
2025-11-27 19:26:19 +0200

    feat: init project with health endpoint

---
+++ app/main.py
@@ -0,0 +1,10 @@
+"""Test service for the shipping"""
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/health")
+def health():
+    """Такая вот заглушка"""
+    return {"status": "ok"}
