3c6be9093e2aa66e069d8006e2913a89725e7879
yuramayer <votetopovorot2015@gmail.com>
2025-12-04 21:46:07 +0200

    feat: read config.toml add config_message & endpoint

--- app/main.py
+++ app/main.py
@@ -6,11 +6,17 @@ import tomllib
 app = FastAPI()
 
 
+with open('config.toml', 'rb') as f:
+    config = tomllib.load(f)
+
+
 @app.get("/health")
 def health():
     """Такая вот заглушка"""
     return {"status": "ok"}
 
 
-with open('config.toml', 'rb') as f:
-    config = tomllib.load(f)
+@app.get('/config_message')
+def config_message():
+    """Месседж из конфига"""
+    return {'msg': config['server']['message']}
