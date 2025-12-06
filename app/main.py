e2bebdeb38dd781e3a0ce608437421095199cd82
yuramayer <votetopovorot2015@gmail.com>
2025-12-06 12:15:34 +0200

    feat: added basic request logging

--- app/main.py
+++ app/main.py
@@ -1,13 +1,31 @@
 """Test service for the shipping"""
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import tomllib
+import logging
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
+
+logger.info('mini-service started')
 
 
 with open('config.toml', 'rb') as f:
     config = tomllib.load(f)
+
+
+@app.middleware('http')
+async def log_requests(request: Request, call_next):
+    logger.info(f' >> {request.method} {request.url}')
+    response = await call_next(request)
+    logger.info(f' << {response.status_code} {request.url}')
+    return response
 
 
 @app.get("/health")
